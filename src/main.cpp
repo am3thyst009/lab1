c0fc4491adb2181cc67d95856b69f1ddebbb7757
am3thyst009 <am3thyst009@gmail.com>
2025-03-02 22:14:47 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 
 int add(int a, int b);
 int subtract(int a, int b);
+int multiply(int a, int b);
 
 int main() {
     int a = 10, b = 5;
 
     std::cout << add(a, b) << std::endl;
     std::cout << subtract(a, b) << std::endl;
+    std::cout << multiply(a, b) << std::endl;
 
     return 0;
 }
