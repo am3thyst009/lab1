e044b617bb26267fc9fca46cbbd1f53bbdd143c6
am3thyst009 <am3thyst009@gmail.com>
2025-03-02 22:05:41 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+int main() {
+    int a = 10, b = 5;
+
+    std::cout << add(a, b) << std::endl;
+    std::cout << subtract(a, b) << std::endl;
+
+    return 0;
+}
